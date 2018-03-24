3ec088c37c91eb2797970a05a2831ea41134d659
Abraham De Alba <adealba@csumb.edu>
2018-03-24 20:52:47 +0000

    Lab5

---
+++ HOMEWORKS/Homework3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> WHICH STAR TREK CAPTAIN ARE YOU? QUIZ!! </title>
+        <meta charset="utf-8" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        
+        <style>
+            body
+            {
+                background-image: url("img/captains.jpg"); background-size: 100%;
+            }
+        </style>
+        
+    </head>
+    
+    <body>
+        <header> WHICH STAR TREK CAPTAIN ARE YOU? QUIZ!! </header>
+        
+        <main>
+            <?php
+                include 'theQuiz.php';
+                play();
+                
+            ?>
+        </main>
+    </body>
+    
+</html>
